a1e09cdfcf4aba214c5bf7a2dbb28dd403e40386
Samuel-3004 <samuelszportela@hotmail.com>
2023-12-01 12:55:12 -0300

    feat: global styling initial adjustment

---
+++ src/styles/globalStyle.ts
@@ -0,0 +1,12 @@
+import { createGlobalStyle } from "styled-components";
+
+export const GlobalStyled = createGlobalStyle`
+* {
+  font-family: "Roboto", sans-serif;
+  box-sizing: border-box;
+}
+body {
+  background-color: #f6f8fc;
+}
+
+`;
